39c6c813ee17ae34c7ed57bee4d2cbfd8f6171a3
ArshadX <mas12542@outlook.com>
2023-09-10 15:17:19 +0530

    api test6

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,21 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const headers: HeadersInit = [
-  ["Accept", "*/*"],
-  ["content-type", "application/json"],
-  ["Access-Control-Request-Headers", "*"],
-  ["api-key", `${process.env.DB_Auth}`],
-];
-const bodyGET = {
-  collection: "emailList",
-  database: "subscriptions",
-  dataSource: "Cluster0",
-  filter: { id: 1 },
-};
-
 export async function GET(request: Request) {
   console.log("GET");
-
+  const headers: HeadersInit = [
+    ["Accept", "*/*"],
+    ["content-type", "application/json"],
+    ["Access-Control-Request-Headers", "*"],
+    ["api-key", `${process.env.DB_Auth}`],
+  ];
+  const bodyGET = {
+    collection: "emailList",
+    database: "subscriptions",
+    dataSource: "Cluster0",
+    filter: { id: 1 },
+  };
   const res = await fetch(`${process.env.baseUrl}/action/findOne`, { headers, body: JSON.stringify(bodyGET), method: "POST" });
   if (res.ok) {
     const data = await res.json();
@@ -25,7 +23,7 @@ export async function GET(request: Request) {
 }
 
 // export async function POST(request: Request) {
-//   console.log("POST");
+//
 //   const { searchParams } = new URL(request.url);
 //   const email = searchParams.get("email");
 //   const bodyPOST = {
@@ -44,3 +42,28 @@ export async function GET(request: Request) {
 //     .catch((err) => err.json());
 //   return NextResponse.json(res);
 // }
+
+export async function POST(request: Request) {
+  console.log("POST");
+  const { searchParams } = new URL(request.url);
+  const email = searchParams.get("email");
+  const res = await fetch(`${process.env.baseUrl}/action/updateOne`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "api-Key": process.env.DB_Auth,
+      "Access-Control-Request-Headers": "*",
+      Accept: "*/*",
+    },
+    body: JSON.stringify({
+      collection: "emailList",
+      database: "subscriptions",
+      dataSource: "Cluster0",
+      filter: { id: 1 },
+      update: { $push: { data: email } },
+    }),
+  });
+  const data = await res.json();
+
+  return NextResponse.json(data);
+}
